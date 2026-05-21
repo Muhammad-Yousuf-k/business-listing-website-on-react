47663f3ac899ed13d009fec8a1deb5e8e5a1b259
Muhammad Yousuf <muhammad.yousuf8166@gmail.com>
2026-05-21 23:43:36 +0500

    fix production api url and cors

--- backend/app.js
+++ backend/app.js
@@ -29,6 +29,10 @@ app.set("trust proxy", 1);
 app.use(
     cors({
         origin: env.clientUrl,
+        origin: [
+            env.clientUrl,
+            "https://6a0f4e5637226fb3208574a0--rankeats.netlify.app",
+        ],
         credentials: true,
     })
 );
--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -8,7 +8,7 @@ export const env = {
   clientUrl: process.env.CLIENT_URL || "http://localhost:5173",
   mongoUri: process.env.MONGO_URI || "mongodb://localhost:27017/rankeats",
   jwtSecret: process.env.JWT_SECRET || "mySuperSecretKey123",
-  
+
   smtpHost: process.env.SMTP_HOST || "smtp.gmail.com",
   smtpPort: process.env.SMTP_PORT || 465,
   smtpUser: process.env.SMTP_USER || "",
